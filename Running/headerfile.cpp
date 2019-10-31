6a93331a5b0fbb0faa5bfea66c8458199421827a
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2019-10-31 10:51:01 +0600

    added new file

---
+++ Running/headerfile.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define  sz    100001
+#define  ll    long long
+#define  pb    push_back
+#define  _mp    make_pair
+#define  ff     first
+#define  ss     second
+#define  all(x)   x.begin(),x.end()
+
+using namespace std;
+vector<ll>vec;
+map<ll,ll>mp;
+long long n,m,i,j;
+
+int main()
+{
+    while(1)
+    cout<<"bou"<<endl;
+  return 0;
+}
