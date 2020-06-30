9508542a82262aa2be1eb7cec68ecdd47955ac3f
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-06-30 11:23:23 -0700

    updated

---
+++ DevSkill/Problem solving Practice/Problem solving Practice/CF1335C.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+
+namespace Problem_solving_Practice
+{
+    class CF1335C
+    {
+        public static void Main(string[] args)
+        {
+            int test = int.Parse(Console.ReadLine());
+            while (test-- > 0)
+            {
+                int n = int.Parse(Console.ReadLine());
+                var line = Console.ReadLine().Split(' ');
+                var map = new Dictionary<int, int>();
+                for(int i = 0; i < n; i++)
+                {
+                    int val = int.Parse(line[i]);
+                    if (map.ContainsKey(val))
+                    {
+                        map[val]++;
+                    }
+                    else
+                        map[val] = 1;
+                }
+                int uniq = map.Count-1;
+                int maxoccur = -1;
+                //Console.WriteLine(uniq);
+                foreach (var item in map)
+                {
+                     maxoccur = Math.Max(maxoccur,item.Value);
+                    //Console.WriteLine($"{item.Key} ");
+                }
+                 
+                if (Math.Abs(uniq - maxoccur) > 1) uniq += 1;
+                int ans = Math.Min(uniq, maxoccur);
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
