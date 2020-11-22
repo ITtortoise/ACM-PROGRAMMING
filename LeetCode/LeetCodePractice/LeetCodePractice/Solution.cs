6850d5ea00f21e9c2947053327c493f9be3be605
SohagSadaf <38885449+sohag16030@users.noreply.github.com>
2020-11-22 13:50:51 +0600

    leet code practice added

---
+++ LeetCode/LeetCodePractice/LeetCodePractice/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodePractice
+{
+    public class Solution
+    {
+
+        static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            var arr = new List<bool>();
+            int maxValue = candies.Max();
+            //int maxIndex = candies.ToList().IndexOf(maxValue);
+            for (int i = 0; i < candies.Length; i++)
+            {             
+                int expected = extraCandies + candies[i];
+                if (expected >= maxValue)
+                    arr.Add(true);
+                else
+                    arr.Add(false);                               
+            }
+            return (arr);
+        }
+        public static void Main()
+        {
+            int[] candies = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+            int extraCandies = int.Parse(Console.ReadLine());
+            KidsWithCandies(candies, extraCandies);
+        }
+    }
+}
